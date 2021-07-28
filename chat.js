e7ec969ca7a333c7f8e7fa3e0814ac3a7829f00a
Jan Poonthong <janpoonthong628@gmail.com>
2021-07-28 16:55:27 +0700

    first commit

---
+++ chat.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const tmi = require("tmi.js");
+const fs = require("fs");
+const path = require("path");
+const SECRET = getSecret();
+const client = new tmi.Client({
+  connection: {
+    reconnect: false,
+    secure: true,
+  },
+  identity: {
+    username: "janpoonthong",
+    password: `oauth:${SECRET}`,
+  },
+});
+
+function main() {
+  let args = process.argv.slice(2);
+  if (args.length !== 1) {
+    console.log("You need atleast one argument");
+    process.exit(1);
+  }
+  let message = args[0];
+  sendMessage(message);
+}
+
+function sendMessage(message) {
+  client.connect();
+  client.on("connected", () => {
+    client.say("#janpoonthong", message);
+    client.disconnect();
+  });
+}
+
+function getSecret() {
+  let secretFilePath = path.join(path.dirname(__filename), "SECRET");
+  return fs.readFileSync(secretFilePath, "utf-8").trim();
+}
+
+main();
